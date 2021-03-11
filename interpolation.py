b7f2ea7f52acf2b1c925abe6f722a791fbd41923
PFNK <m.sliacka12@gmail.com>
2021-03-11 14:52:26 +0000

    few changes to interpolation stuff

--- interpolation.py
+++ interpolation.py
@@ -42,41 +42,21 @@ def interpolation(
     verbose                    = False,
     device: torch.device
 ):
-'''
-    input hair_img, person_img
-
-    hair_img -> masked_hair_img
-    person_img -> masked_person_img
-    masked_person_img -> vgg -> person_features
-    masked_hair_img -> vgg -> hair_features
-
-    masked_imgs -> projection -> z_h, z_p -> mapping network -> w_h, w_p 
-    disable grads for G
-    define random Q requires_grad
-    optimizer loop:
-        w_t <- w_h + w_p
-        G.synthesis(w_t) -> synth_image
-        synth_img -> segmentation -> masked_hair_synth_img
-        masked_hair_synth_img -> vgg -> masked_hair_synth_feat
-        loss:
-            (masked_hair_synth_feat - hair_features).square.sum for each
-            define alphas
-
-'''
-
+    print("inside interpolation function")
     assert identity.shape == (G.img_channels, G.img_resolution, G.img_resolution)
 
     def logprint(*args):
         if verbose:
             print(*args)
 
+    print("loading G")
     G = copy.deepcopy(G).eval().requires_grad_(False).to(device) # type: ignore
 
     # Compute q stats.
     logprint(f'Computing W midpoint and stddev using {q_avg_samples} samples...')
     q_samples = np.random.RandomState(123).randn(q_avg_samples, G.w_dim)  
     q_avg = np.mean(q_samples, axis=0, keepdims=True)     # [G.w_dim]
-    q_std = (np.sum((q_samples - q_avg) ** 2) / w_avg_samples) ** 0.5
+    q_std = (np.sum((q_samples - q_avg) ** 2) / q_avg_samples) ** 0.5
 
     # Setup noise inputs.
     noise_bufs = { name: buf for (name, buf) in G.synthesis.named_buffers() if 'noise_const' in name }
@@ -85,7 +65,7 @@ def interpolation(
     # url = 'https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/metrics/vgg16.pt'
     # with dnnlib.util.open_url(url) as f:
     #     vgg16 = torch.jit.load(f).eval().to(device)
-
+    print("loading vgg")
     vgg16 = torch.jit.load('vgg16.pt').eval().to(device)
 
     # TODO: img -> masked_img
@@ -96,17 +76,21 @@ def interpolation(
     masked_identity_img = identity.unsqueeze(0).to(device).to(torch.float32)
     if masked_identity_img.shape[2] > 256:
         masked_identity_img = F.interpolate(masked_identity_img, size=(256, 256), mode='area')
+    print("running identity through vgg")
     identity_features = vgg16(masked_identity_img, resize_images=False, return_lpips=True)
 
     # Features for hair image.
     masked_hair_img = hair.unsqueeze(0).to(device).to(torch.float32)
     if masked_hair_img.shape[2] > 256:
         masked_hair_img = F.interpolate(masked_hair_img, size=(256, 256), mode='area')
+    print("running hair through vgg")
     hair_features = vgg16(masked_hair_img, resize_images=False, return_lpips=True)
 
     # Projection of images to w
-    w_h = project(G, hair)
-    w_p = project(G, identity)
+    print("HARI SHAPE ", hair.shape)
+    w_h = project(G, hair, device=torch.device('cuda'))
+    print("w_h shape: ", w_h.shape)
+    w_p = project(G, identity, device=torch.device('cuda'))
 
     q_opt = torch.tensor(q_avg, dtype=torch.float32, device=device, requires_grad=True) # pylint: disable=not-callable
     # list of all target ws through optimization
@@ -117,8 +101,9 @@ def interpolation(
     for buf in noise_bufs.values():
         buf[:] = torch.randn_like(buf)
         buf.requires_grad = True
-
+    print("starting the iterations..")
     for step in range(num_steps):
+        print("iteration ", step)
         # Learning rate schedule.
         t = step / num_steps
         q_noise_scale = q_std * initial_noise_factor * max(0.0, 1.0 - t / noise_ramp_length) ** 2
@@ -146,6 +131,7 @@ def interpolation(
         hair_target_image = target_image
         identity_target_image = target_image
 
+        print("running target features through vgg")
         # Features for synth images.
         target_hair_features = vgg16(hair_target_image, resize_images=False, return_lpips=True)
         target_identity_features = vgg16(identity_target_image, resize_images=False, return_lpips=True)
@@ -168,6 +154,7 @@ def interpolation(
         loss = dist + reg_loss * regularize_noise_weight
 
         # Step
+        print("optimizer steps")
         optimizer.zero_grad(set_to_none=True)
         loss.backward()
         optimizer.step()
@@ -181,21 +168,23 @@ def interpolation(
             for buf in noise_bufs.values():
                 buf -= buf.mean()
                 buf *= buf.square().mean().rsqrt()
-
+    print("Done with interpolation.")
     return w_out
 
 #----------------------------------------------------------------------------
 
 @click.command()
 @click.option('--network', 'network_pkl', help='Network pickle filename', required=True)
-@click.option('--target', 'target_fname', help='Target image file to project to', required=True, metavar='FILE')
+@click.option('--hair', 'hair_fname', required=True, metavar='FILE')
+@click.option('--identity', 'identity_fname', required=True, metavar='FILE')
 @click.option('--num-steps',              help='Number of optimization steps', type=int, default=1000, show_default=True)
 @click.option('--seed',                   help='Random seed', type=int, default=303, show_default=True)
 @click.option('--save-video',             help='Save an mp4 video of optimization progress', type=bool, default=True, show_default=True)
 @click.option('--outdir',                 help='Where to save the output images', required=True, metavar='DIR')
 def run_projection(
     network_pkl: str,
-    target_fname: str,
+    hair_fname: str,
+    identity_fname: str,
     outdir: str,
     save_video: bool,
     seed: int,
@@ -218,19 +207,27 @@ def run_projection(
     with dnnlib.util.open_url(network_pkl) as fp:
         G = legacy.load_network_pkl(fp)['G_ema'].requires_grad_(False).to(device) # type: ignore
 
-    # Load target image.
-    target_pil = PIL.Image.open(target_fname).convert('RGB')
-    w, h = target_pil.size
+    # Load hair image.
+    hair_pil = PIL.Image.open(hair_fname).convert('RGB')
+    w, h = hair_pil.size
     s = min(w, h)
-    target_pil = target_pil.crop(((w - s) // 2, (h - s) // 2, (w + s) // 2, (h + s) // 2))
-    target_pil = target_pil.resize((G.img_resolution, G.img_resolution), PIL.Image.LANCZOS)
-    target_uint8 = np.array(target_pil, dtype=np.uint8)
+    hair_pil = hair_pil.crop(((w - s) // 2, (h - s) // 2, (w + s) // 2, (h + s) // 2))
+    hair_pil = hair_pil.resize((G.img_resolution, G.img_resolution), PIL.Image.LANCZOS)
+    hair_uint8 = np.array(hair_pil, dtype=np.uint8)
+
+    identity_pil = PIL.Image.open(identity_fname).convert('RGB')
+    w, h = identity_pil.size
+    s = min(w, h)
+    identity_pil = identity_pil.crop(((w - s) // 2, (h - s) // 2, (w + s) // 2, (h + s) // 2))
+    identity_pil = identity_pil.resize((G.img_resolution, G.img_resolution), PIL.Image.LANCZOS)
+    identity_uint8 = np.array(identity_pil, dtype=np.uint8)
 
     # Optimize projection.
     start_time = perf_counter()
-    projected_w_steps = project(
+    projected_w_steps = interpolation(
         G,
-        target=torch.tensor(target_uint8.transpose([2, 0, 1]), device=device), # pylint: disable=not-callable
+        hair=torch.tensor(hair_uint8.transpose([2, 0, 1]), device=device), # pylint: disable=not-callable
+        identity=torch.tensor(identity_uint8.transpose([2, 0, 1]), device=device),
         num_steps=num_steps,
         device=device,
         verbose=True
@@ -246,11 +243,11 @@ def run_projection(
             synth_image = G.synthesis(projected_w.unsqueeze(0), noise_mode='const')
             synth_image = (synth_image + 1) * (255/2)
             synth_image = synth_image.permute(0, 2, 3, 1).clamp(0, 255).to(torch.uint8)[0].cpu().numpy()
-            video.append_data(np.concatenate([target_uint8, synth_image], axis=1))
+            video.append_data(np.concatenate([identity_uint8, synth_image], axis=1))
         video.close()
 
     # Save final projected frame and W vector.
-    target_pil.save(f'{outdir}/target.png')
+    hair_pil.save(f'{outdir}/target.png')
     projected_w = projected_w_steps[-1]
     synth_image = G.synthesis(projected_w.unsqueeze(0), noise_mode='const')
     synth_image = (synth_image + 1) * (255/2)

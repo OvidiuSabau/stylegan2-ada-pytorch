2bb6a6e33fb24c5f19d417d51f8cf58cfbefe36c
PFNK <m.sliacka12@gmail.com>
2021-03-23 14:26:11 +0000

    adding my 12 female and male 18x512 projections, changes to run interpolation in loop and early stopping

--- interpolation.py
+++ interpolation.py
@@ -9,8 +9,10 @@
 """Project given image to the latent space of pretrained network pickle."""
 
 import copy
+import glob
 import os
 from time import perf_counter
+from pathlib import Path
 
 import click
 import imageio
@@ -21,6 +23,7 @@ import torch.nn.functional as F
 
 import dnnlib
 import legacy
+from projector_18 import project as project_18
 
 
 def interpolation(
@@ -39,7 +42,10 @@ def interpolation(
     noise_ramp_length          = 0.75,
     regularize_noise_weight    = 1e2,
     verbose                    = False,
-    device: torch.device
+    stop,
+    device: torch.device,
+    hair_img_filename,
+    identity_img_filename
 ):
 
 
@@ -105,19 +111,25 @@ def interpolation(
     hair_features = vgg16(masked_hair_img, resize_images=False, return_lpips=True)
 
     # Loading the projection of images to save time when debugging
-    w_h = torch.from_numpy(np.load("projected_w_p.npz")['w']).to('cuda')  # (18,512)
-    w_p = torch.from_numpy(np.load("projected_w_h.npz")['w']).to('cuda')  # (18,512)
-    print(w_h.shape)
-    # w_h = project(G, hair, device=torch.device('cuda'))[-1]
-    # w_p = project(G, identity, device=torch.device('cuda'))[-1]
-    #
-    # np.savez(f'projected_w_h.npz', w=w_h.cpu().numpy())
-    # np.savez(f'projected_w_p.npz', w=w_p.cpu().numpy())
+    w_h_path = Path("projections_18x512/{}.npz".format(hair_img_filename))
+    w_p_path = Path("projections_18x512/{}.npz".format(identity_img_filename))
+
+    if w_h_path.exists():
+        w_h = torch.from_numpy(np.load(w_h_path)['w']).to('cuda')
+    else:
+        w_h = project_18(G, hair, device=torch.device('cuda'))[-1]
+        np.savez(w_h_path, w=w_h.cpu().numpy())
+
+    if w_p_path.exists():
+        w_p = torch.from_numpy(np.load(w_p_path)['w']).to('cuda')
+    else:
+        w_p = project_18(G, identity, device=torch.device('cuda'))[-1]
+        np.savez(w_p_path, w=w_p.cpu().numpy())
 
     q_opt = torch.nn.Parameter(torch.randn(size=w_p.shape, dtype=torch.float32, requires_grad=True, device=device))
 
     # list of all target ws through optimization
-    w_out = torch.zeros([num_steps] + list(w_h.shape), dtype=torch.float32, device=device)
+    w_out = torch.zeros([stop] + list(w_h.shape), dtype=torch.float32, device=device)
     optimizer = torch.optim.Adam([q_opt] + list(noise_bufs.values()), betas=(0.9, 0.999), lr=initial_learning_rate)
 
     # Init noise.
@@ -127,6 +139,7 @@ def interpolation(
     print("starting the iterations..")
     for step in range(num_steps):
         print("iteration ", step)
+        if step==stop: break
         # Learning rate schedule.
         t = step / num_steps
         w_noise_scale = w_std * initial_noise_factor * max(0.0, 1.0 - t / noise_ramp_length) ** 2
@@ -196,21 +209,22 @@ def interpolation(
 
 #----------------------------------------------------------------------------
 
-@click.command()
-@click.option('--network', 'network_pkl', help='Network pickle filename', required=True)
-@click.option('--hair', 'hair_fname', required=True, metavar='FILE')
-@click.option('--identity', 'identity_fname', required=True, metavar='FILE')
-@click.option('--num-steps',              help='Number of optimization steps', type=int, default=500, show_default=True)
-@click.option('--seed',                   help='Random seed', type=int, default=303, show_default=True)
-@click.option('--save-video',             help='Save an mp4 video of optimization progress', type=bool, default=True, show_default=True)
-@click.option('--outdir',                 help='Where to save the output images', required=True, metavar='DIR')
-def run_projection(
+# @click.command()
+# @click.option('--network', 'network_pkl', help='Network pickle filename', required=True)
+# @click.option('--hair', 'hair_fname', required=True, metavar='FILE')
+# @click.option('--identity', 'identity_fname', required=True, metavar='FILE')
+# @click.option('--num-steps',              help='Number of optimization steps', type=int, default=500, show_default=True)
+# @click.option('--seed',                   help='Random seed', type=int, default=303, show_default=True)
+# @click.option('--save-video',             help='Save an mp4 video of optimization progress', type=bool, default=True, show_default=True)
+# @click.option('--outdir',                 help='Where to save the output images', required=True, metavar='DIR')
+def run_interpolation(
     network_pkl: str,
     hair_fname: str,
     identity_fname: str,
     outdir: str,
     save_video: bool,
     seed: int,
+    stop: int,
     num_steps: int
 ):
     """Project given image to the latent space of pretrained network pickle.
@@ -237,6 +251,7 @@ def run_projection(
     hair_pil = hair_pil.crop(((w - s) // 2, (h - s) // 2, (w + s) // 2, (h + s) // 2))
     hair_pil = hair_pil.resize((G.img_resolution, G.img_resolution), PIL.Image.LANCZOS)
     hair_uint8 = np.array(hair_pil, dtype=np.uint8)
+    hair_img_filename = hair_fname.split('/')[1].split('.')[0]
 
     identity_pil = PIL.Image.open(identity_fname).convert('RGB')
     w, h = identity_pil.size
@@ -244,6 +259,7 @@ def run_projection(
     identity_pil = identity_pil.crop(((w - s) // 2, (h - s) // 2, (w + s) // 2, (h + s) // 2))
     identity_pil = identity_pil.resize((G.img_resolution, G.img_resolution), PIL.Image.LANCZOS)
     identity_uint8 = np.array(identity_pil, dtype=np.uint8)
+    identity_img_filename = identity_fname.split('/')[1].split('.')[0]
 
     # Optimize projection.
     start_time = perf_counter()
@@ -253,14 +269,17 @@ def run_projection(
         identity=torch.tensor(identity_uint8.transpose([2, 0, 1]), device=device),
         num_steps=num_steps,
         device=device,
-        verbose=True
+        verbose=True,
+        stop=stop,
+        hair_img_filename=hair_img_filename,
+        identity_img_filename=identity_img_filename
     )
     print (f'Elapsed: {(perf_counter()-start_time):.1f} s')
 
     # Render debug output: optional video and projected image and W vector.
     os.makedirs(outdir, exist_ok=True)
     if save_video:
-        video = imageio.get_writer(f'{outdir}/proj.mp4', mode='I', fps=10, codec='libx264', bitrate='16M')
+        video = imageio.get_writer('{}/proj_i{}_h{}.mp4'.format(outdir, identity_img_filename, hair_img_filename), mode='I', fps=10, codec='libx264', bitrate='16M')
         print (f'Saving optimization progress video "{outdir}/proj.mp4"')
         for projected_w in projected_w_steps:
             synth_image = G.synthesis(projected_w.unsqueeze(0), noise_mode='const')
@@ -270,18 +289,71 @@ def run_projection(
         video.close()
 
     # Save final projected frame and W vector.
-    hair_pil.save(f'{outdir}/hair.png')
-    identity_pil.save(f'{outdir}/identity.png')
+    # hair_pil.save('{}/hair_{}.png'.format(outdir, hair_img_filename))
+    # identity_pil.save('{}/identity_{}.png'.format(outdir, identity_img_filename))
     projected_w = projected_w_steps[-1]
     synth_image = G.synthesis(projected_w.unsqueeze(0), noise_mode='const')
     synth_image = (synth_image + 1) * (255/2)
     synth_image = synth_image.permute(0, 2, 3, 1).clamp(0, 255).to(torch.uint8)[0].cpu().numpy()
-    PIL.Image.fromarray(synth_image, 'RGB').save(f'{outdir}/synth.png')
-    np.savez(f'{outdir}/synth_w.npz', w=projected_w.unsqueeze(0).cpu().numpy())
+    PIL.Image.fromarray(synth_image, 'RGB').save('{}/synth_i{}_h{}.png'.format(outdir, identity_img_filename, hair_img_filename))
+    np.savez('{}/synth_w_i{}_h{}.npz'.format(outdir, identity_img_filename, hair_img_filename), w=projected_w.unsqueeze(0).cpu().numpy())
 
 #----------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    run_projection() # pylint: disable=no-value-for-parameter
+    imgs_paths = glob.glob("male_6/*.jpg")
+    for img1 in imgs_paths:
+        for img2 in imgs_paths:
+            if img1 == img2: continue
+            run_interpolation(hair_fname=img1,
+                              identity_fname=img2,
+                              network_pkl="ffhq.pkl",
+                              outdir="output_dir",
+                              save_video=False,
+                              num_steps=300,
+                              stop=100,
+                              seed=303)
+
+    max_horizontal_imgs = 6
+    img_paths = sorted(glob.glob("male_q512/*.png"))
+    order = sorted([img_p.split('.')[0] for img_p in glob.glob("male_6/*.jpg")])
+    first_imgs = [PIL.Image.open(path+".jpg").convert('RGB') for path in order]
+    white_img = PIL.Image.new("RGB", first_imgs[0].size, (255,255,255))
+    imgs=[]
+
+    for i, img_p in enumerate(img_paths):
+        if i % max_horizontal_imgs == 0:
+            imgs.append(white_img)
+        img = PIL.Image.open(img_p).convert('RGB')
+        img_id = img_p.split('/')[-1].split('.')[0].split('synth_')[-1]
+        imgs.append(img)
+    imgs.append(white_img)
+
+    def pil_grid(images, max_horiz=np.iinfo(int).max):
+        n_images = len(images)
+        n_horiz = min(n_images, max_horiz)
+        h_sizes, v_sizes = [0] * n_horiz, [0] * (n_images // n_horiz)
+        for i, im in enumerate(images):
+            h, v = i % n_horiz, i // n_horiz
+            h_sizes[h] = max(h_sizes[h], im.size[0])
+            v_sizes[v] = max(v_sizes[v], im.size[1])
+        h_sizes, v_sizes = np.cumsum([0] + h_sizes), np.cumsum([0] + v_sizes)
+        im_grid = PIL.Image.new('RGB', (h_sizes[-1], v_sizes[-1]), color='white')
+        for i, im in enumerate(images):
+            im_grid.paste(im, (h_sizes[i % n_horiz], v_sizes[i // n_horiz]))
+        return im_grid
+
+    grid = pil_grid(imgs, max_horiz=max_horizontal_imgs)
+    # create first row
+    first_row = pil_grid(first_imgs, max_horiz=len(first_imgs))
+    # create first column
+    first_imgs.insert(0, white_img)
+    first_column = pil_grid(first_imgs, max_horiz=1)
+    # append first row to grid
+    grid = pil_grid([first_row, grid], max_horiz=1)
+    # append first column
+    grid = pil_grid([first_column, grid], max_horiz=2)
+
+    grid.save("male_q512.png")
 
 #----------------------------------------------------------------------------
